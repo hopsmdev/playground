2fcf642791b1e0170d470d27ed19aa7611d106ab
hopsm.dev <hopsm.dev@gmail.com>
2016-01-03 15:20:36 +0100

    add download sequential test

---
+++ concurrent_ futures_test.py
@@ -0,0 +1,33 @@
+import requests
+from concurrent import futures
+from execution_time import execution_time
+
+MAX_WORKERS = 32
+
+things_to_download = [
+    'http://news.ycombinator.com/rss',
+    'http://www.reddit.com/r/Python/.rss',
+]
+
+
+def get_thing(thing):
+    return requests.get(thing).text
+
+
+@execution_time(loops=10, verbose=False)
+def download_all_things(things):
+    workers = min(MAX_WORKERS, len(things))
+    with futures.ThreadPoolExecutor(max_workers=workers) as executor:
+        response = executor.map(get_thing, things)
+    return response
+
+
+@execution_time(loops=10, verbose=False)
+def download_all_things_sequential(things):
+    for thing in things:
+        get_thing(thing)
+
+
+if __name__ == "__main__":
+    download_all_things(things_to_download)
+    download_all_things_sequential(things_to_download)
