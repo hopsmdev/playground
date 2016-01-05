95ed90ab6bfe7deaecccf3dd7d10279a7305126e
hopsm.dev <hopsm.dev@gmail.com>
2016-01-05 17:32:41 +0100

    fix command order in menu

--- gui-tk/menu.py
+++ gui-tk/menu.py
@@ -4,8 +4,8 @@ from functools import partial
 from collections import OrderedDict
 
 
-def add_menu(handler, cascade=True, label=None):
-    menu = Menu(handler)
+def add_menu(handler, cascade=True, label=None, **kwargs):
+    menu = Menu(handler, tearoff=0, **kwargs)
     if cascade:
         handler.add_cascade(label=label, menu=menu)
     return handler, menu
@@ -55,17 +55,17 @@ def main():
     root, file_menu = add_menu(menu, label='File')
     root, help_menu = add_menu(menu, label='Help')
 
-    file_commands = OrderedDict(
-        New=Command.new,
-        Open=Command.open,
-        Save=Command.save,
-        Exit=partial(Command.exit, root),
-    )
+    file_commands = OrderedDict([
+        ('New', Command.new),
+        ('Open', Command.open),
+        ('Save', Command.save),
+        ('Exit',partial(Command.exit, root)),
+    ])
 
-    help_commands = OrderedDict(
-        Help=Command.help,
-        About=Command.about,
-    )
+    help_commands = OrderedDict([
+        ('Help', Command.help),
+        ('About', Command.about),
+    ])
 
     add_commands(file_menu, file_commands)
     add_commands(help_menu, help_commands)
